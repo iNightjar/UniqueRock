76b6cac7b829ec876783b7f076e5ce2521810114
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-09-25 10:34:49 +0200

    Backing up /tmp/dnf file

--- archive-dnf.sh
+++ archive-dnf.sh
@@ -3,8 +3,8 @@
 # backing-up /tmp/dnf file, if exists replace with newer verion and store the older with .old backup file
 
 if test -f /tmp/archive.tar.gz; then
-	mv /tmp/archive.tar.gz /tmp/archive.tar.gz.old
-	tar acf /tmp/archive.tar.gz /etc/dnf
+	mv /tmp/archive.tar.gz /tmp/archive.tar.gz.OLD
+	tar acf /tmp/archive.tar.gz /etc/dnf/
 else
-	tar acf /tmp/archive.tar.gz /tmp/dnf
-if
+	tar acf /tmp/archive.tar.gz /etc/dnf/
+fi
